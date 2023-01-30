1534205fff3948d5a3c28e517d467c234349815d
JoshCodesStuff <50310009+JoshCodesStuff@users.noreply.github.com>
2023-01-30 21:26:44 +1030

    Update clean_data.py
    Removed the emoticon part for efficiency (now takes approx 18 seconds to run) and restructured the method for removing unnecessary characters

--- clean_data.py
+++ clean_data.py
@@ -3,70 +3,56 @@ import sys
 import csv
 from time import perf_counter
 
-def remove_emoji(string):
-    print("Called remove_emoji()")
-    emoji_pattern = re.compile("["
-                               u"\U0001F600-\U0001F64F"  # emoticons
-                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                               u"\U00002500-\U00002BEF"  # chinese char
-                               u"\U00002702-\U000027B0"
-                               u"\U00002702-\U000027B0"
-                               u"\U000024C2-\U0001F251"
-                               u"\U0001f926-\U0001f937"
-                               u"\U00010000-\U0010ffff"
-                               u"\u2640-\u2642"
-                               u"\u2600-\u2B55"
-                               u"\u200d"
-                               u"\u23cf"
-                               u"\u23e9"
-                               u"\u231a"
-                               u"\ufe0f"  # dingbats
-                               u"\u3030"
-                               "]+", flags=re.UNICODE)
-    return emoji_pattern.sub(r'', string)
-
 def clean_csv(filename='.\data.csv', TRACE=0):
     """
     Cleans CSV files for nice readable formats.
     - Takes [filename | string] for the working directory.
     - Takes [TRACE | int] to check whether we are in debug mode.
     """
+    print('cleaning', filename)
+
     # check performance
     start = perf_counter()
     
-    if TRACE==1:
-        print('cleaning', filename)
 
     c_lines = 0
     with open(filename, encoding='utf-8') as f_in, \
     open ('clean_data.csv', 'w', newline='', encoding='utf-8') as f_out:
         print(f"File opened: {filename}")
 
-        c_in = csv.reader(f_in)
-        c_out = csv.writer(f_out)
+        csv_in = csv.reader(f_in)
+        csv_out = csv.writer(f_out)
 
-        for row in c_in:
-            if len(row) == 5:
-                row[4] = row[4].replace('\n', '').replace('Â', ' ').replace('\n\n', '').replace("â€", "'")
-                c_out.writerow(row)
-
+        n = 0
+        for row in csv_in:
+            if len(row) > 1:
+                row[4] = re.sub("[\xa0\n]", ' ', row[4]) # remove \n and unicode char
+                row[4] = re.sub("[<…>]", '', row[4])
+                row[4] = row[4].replace('.', '. ')
+                csv_out.writerow(row)
+            
+            x = row[4].split('\n')
+            if len(x) > 1:
+                print("ERROR: length of string off by too far.")
+                sys.exit()
+            n+=1
     
     # end timer check
     finished = perf_counter() - start
     
-    print(f'Finished cleaning in {finished:.5f} seconds')
+    print(f'Finished cleaning {n} rows in {finished:.6f} seconds')
 
 
 if __name__ == "__main__":
     maxInt = sys.maxsize
+
+    # decrease maxInt value while OverflowError occurs.
     while True:
-        # decrease the maxInt value by factor 10 
-        # as long as the OverflowError occurs.
         try:
             csv.field_size_limit(maxInt)
             break
         except OverflowError:
             maxInt = int(maxInt/10)
+    
+    # clean the csv
     clean_csv(TRACE=1)
